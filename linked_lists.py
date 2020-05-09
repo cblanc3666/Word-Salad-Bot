eb275df4c818848e1c5ef1d6da6be2c7e652f48e
cwduffy01 <31222907+cwduffy01@users.noreply.github.com>
2020-05-09 02:34:34 -0400

    Add files via upload

--- linked_lists.py
+++ linked_lists.py
@@ -21,16 +21,25 @@ class Node:
         return self.data
 
 
+class Node:
+    def __init__(self, data):
+        self.data = data    # the value the Node carries
+        self.next = None    # a Node
+
+    def __repr__(self):
+        return self.data
+
+
 class CircularLinkedList:
     def __init__(self, nodes=None):
         self.head = None    # a Node
-        if nodes is not None:
+        if nodes is not None:   # iterates through iterable nodes and links one to the next
             node = Node(data=nodes.pop(0))
             self.head = node
             for elem in nodes:
                 node.next = Node(data=elem)
                 node = node.next
-            node.next = self.head
+            node.next = self.head   # links the last node to the first
 
     def __repr__(self):
         node = self.head
@@ -39,7 +48,7 @@ class CircularLinkedList:
         while True:
             nodes.append(node.data)
             node = node.next
-            if node == self.head:
+            if node == self.head:   # if all nodes have been added to list
                 nodes.append(node.data)
                 break
         nodes.append("...")
@@ -50,61 +59,23 @@ class CircularLinkedList:
         while True:
             yield node
             node = node.next
-            if node == self.head:
+            if node == self.head:   # iterate until loop is complete
                 return
 
-    def add_node(self, node):
+    def add_node(self, node):   #adds node to the "end" of the circular linked list
         if not self.head:
             self.head = node
             return
         for current_node in self:
-            if current_node.next == self.head:
-                current_node.next = node
-                node.next = self.head
+            if current_node.next == self.head:  # if the "end" has been reached
+                current_node.next = node    #insert node
+                node.next = self.head   # link back to head
 
-    def remove_node(self, node):
+    def remove_node(self, node):    # remove node from circular linked list
         for current_node in self:
-            if current_node.next.data == node.data:
-                current_node.next = current_node.next.next
+            if current_node.next.data == node.data:     # if target node is found
+                current_node.next = current_node.next.next  # skip over target node
                 return
-
-
-# class LinkedList:
-#     def __init__(self, nodes=None):
-#         self.head = None
-#         if nodes is not None:
-#             node = Node(data=nodes.pop(0))
-#             self.head = node
-#             for elem in nodes:
-#                 node.next = Node(data=elem)
-#                 node = node.next
-#
-#     def __iter__(self):
-#         node = self.head
-#         while node is not None:
-#             yield node
-#             node = node.next
-#
-#     def __repr__(self):
-#         node = self.head
-#         nodes = []
-#         while node is not None:
-#             nodes.append(node.data)
-#             node = node.next
-#         nodes.append("None")
-#         return " -> ".join(nodes)
-#
-#     def add_first(self, node):
-#         node.next = self.head
-#         self.head = node
-#
-#     def add_last(self, node):
-#         if not self.head:
-#             self.head = node
-#             return
-#         for current_node in self:
-#             pass
-#         current_node.next = node
 
 
 main()
