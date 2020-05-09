24485185ea662bf54ce8021fed4959cdef5fc77f
cwduffy01 <31222907+cwduffy01@users.noreply.github.com>
2020-05-09 01:01:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-pool = []
-players = {}
+import random
+
+
+distribution = {'a': 13, 'b': 3, 'c': 3, 'd': 6, 'e': 18, 'f': 3, 'g': 4, 'h': 3, 'i': 12, 'j': 2, 'k': 2, 'l': 5,
+                'm': 3, 'n': 8, 'o': 11, 'p': 3, 'q': 2, 'r': 9, 's': 6, 't': 9, 'u': 6, 'v': 3, 'w': 3, 'x': 2, 'y': 3,
+                'z': 2
+}
 
 
 def main():
-    pass
+    # Fill tile bag
+    tile_bag = []
+    for letter in distribution:
+        for i in range(distribution[letter]):
+            tile_bag.append(letter)
+
+    players = []
+    command = ""
 
 
 # Player has a name and a list of words
 class Player:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, user):
+        self.name = name    # The in-game name a player uses
+        self.user = user    # The discord username of the player
         self.words = []
+        self.vip = False
 
     def add_word(self, word):
         self.words.append(word)
 
     def remove_word(self, word):
         self.words.remove(word)
+
+    def is_vip(self):
+        vip = True
 
 
 # Returns the letters that need to be removed from the pool
